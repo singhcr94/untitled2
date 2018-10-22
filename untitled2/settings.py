a93baa1e22384e1522b718382a62450cb76ad6d6
jaskaran <singhcr94@gmail.com>
2018-10-22 14:37:26 -0700

    changed settings

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$#urv8d4vsf3!!5#))aux_f-t@zwc&64@jd9uopj)(8r=8r#xu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.220.147.41','*']
 
 
 # Application definition
